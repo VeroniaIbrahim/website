53252f644d9dfdc3448b4468e8e6733086e13e54
VeroniaIbrahim <veronia.Ibrahim19@gmail.com>
2024-04-22 01:36:06 +0200

    .

--- src/screens/Hover/Hover.jsx
+++ src/screens/Hover/Hover.jsx
@@ -9,9 +9,8 @@ import { Parameters } from "../../components/Parameters";
 import { SimulationStreaming } from "../../components/SimulationStreaming";
 import { VideoStream } from "../../components/VideoStream";
 import { Amplify } from "aws-amplify";
-import { awsconfig } from "../../aws-exports"
+import  awsconfig  from "../../aws-exports"
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import "./style.css";
 
