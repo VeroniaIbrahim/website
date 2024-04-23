03689087d69efa34fc4118385629216d052f4a2d
VeroniaIbrahim <veronia.Ibrahim19@gmail.com>
2024-04-23 02:21:38 +0200

    l

--- src/screens/Hover/Hover.jsx
+++ src/screens/Hover/Hover.jsx
@@ -25,7 +25,7 @@ export const Hovercomponent = () => {
       console.error("No parameter data to send.");
       return;
     }
-    fetch("https://rq0btgzijg.execute-api.eu-west-3.amazonaws.com/teststage/data2s3", {
+    fetch("https://rq0btgzijg.execute-api.eu-west-3.amazonaws.com/teststage", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json' // Specify content type as JSON ?data=${parameterData}
